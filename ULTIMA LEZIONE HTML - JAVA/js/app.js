f6a293a6f46e5eb138f8e7baf42c79b0d353273b
Helmatron <ema.matro@gmail.com>
2024-03-30 12:53:27 +0100

    all function

--- ULTIMA LEZIONE HTML - JAVA/js/app.js
+++ ULTIMA LEZIONE HTML - JAVA/js/app.js
@@ -51,3 +51,28 @@ const trHTMLString = `
 tBodyElement.innerHTML = trHTMLString
 
 }
+
+const form = document.getElementById("form-add-members")
+
+//Funzione che accetta un parametro di tipo evento
+function aggiungiMembro(e) {
+    //Disabilitare la propagazione del submit del Form che refresha la pagina
+    e.preventDefault(); //disabilita l'evento richiamato
+    
+    //Recuperiamo i singoli imput per ID dal form
+    const name = document.getElementById("name").value1;
+    const job = document.getElementById("role").value;
+    const email = document.getElementById("email").value;
+
+    //con i valori costruiamo l'oggetto "nuovo membro" da pushare
+    const nuovoMembro = {
+        name: name,
+        job: job,
+        email: email,
+    };
+
+    console.log(nuovoMembro)
+
+    //Pushare il nuovo membro nell'array
+    teams.push(nuovoMembro);
+}
